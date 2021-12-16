1ada13cbec686f499cc6eaf6d0f8e6417c02b846
Jey45 <gjeje72@hotmail.fr>
2021-12-16 15:34:33 +0100

    update login page

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,6 +1,7 @@
 import '../styles/globals.css'
+import '../styles/login.css'
 
-function MyApp({ Component, pageProps }) {
+function MyApp ({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
 
