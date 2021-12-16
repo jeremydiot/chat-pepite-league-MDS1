37e7baa3bc5676e2ad380fd03e89ac8775d327f4
DIOT Jeremy <diot.jeremy@outlook.com>
2021-12-16 14:35:26 +0100

    add strapi backend

---
+++ backend/app/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'e157fd08bf4ae167a915b7dfd31d891f'),
+    },
+  },
+});
---
+++ frontend/README.md
@@ -0,0 +1,10 @@
+# pepitchat
+
+Pepite League - My digital school Groupe 1
+
+## Equipe
+- Jérémy Diot
+- Nathan Gonzalez
+- Axel Gonon
+- Jerome Georges
+- Arthur Marionneau
