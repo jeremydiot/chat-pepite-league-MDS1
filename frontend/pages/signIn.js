293baddebb679af52814ee5390123edec00de8c4
NTHGZL <nathan_gonzalez@hotmail.fr>
2021-12-16 16:46:57 +0100

    add register method

--- frontend/pages/signIn.js
+++ frontend/pages/signIn.js
@@ -1,17 +1,47 @@
+
+import axios from "axios";
+import { useState } from "react";
+
 export default function SignIn() {
+
+  const [lastname, setLastname] = useState('')
+  const [firstname, setFirstname] = useState('')
+  const [mail, setMail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleSubmit = async () => {
+    const user = {
+      lastname: "Gonzalez",
+      firstname: "Nathan",
+      email: "nathan@gonzalez.es",
+      password: "test1234",
+      username: "znatan"
+    }
+
+    try {
+      const response = await axios.post("http://localhost:1337/auth/local/register", user)
+      console.log(response.data)
+
+
+    } catch (error) {
+      
+    }
+
+  }
+
   return (
     <div>
-      <form class="container">
+      <div className="container">
         <h2>S'inscrire</h2>
-        <input type="text" placeholder="LastName" />
-        <input type="text" placeholder="FirstName" />
-        <input type="Email" placeholder="Email" />
-        <input type="password" placeholder="password" />
-        <button type="submit" class="btn-form">
+        <input type="text" placeholder="LastName" onChange={lastname => setLastname(lastname.target.value)}/>
+        <input type="text" placeholder="FirstName" onChange={firstname => setFirstname(firstname.target.value)}/>
+        <input type="Email" placeholder="Email" onChange={mail => setMail(mail.target.value)}/>
+        <input type="password" placeholder="password" onChange={password => setPassword(password.target.value)}/>
+        <button className="btn-form" onClick={handleSubmit}>
           S'inscrire
         </button>
-      <span class="logout-btn">Vous n'etes pas encore connecté ? <a href='/login'>Se connecter</a></span>
-      </form>
+      <span className="logout-btn">Vous n'etes pas encore connecté ? <a href='/login'>Se connecter</a></span>
+      </div>
     </div>
   );
 }
